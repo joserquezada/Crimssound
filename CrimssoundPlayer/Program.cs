9a977e49a74e1d36c912a21cda6651f3bf1d132d
jose_r_quezada <jose_r_quezada_z@gmail.com>
2025-05-12 11:35:55 -0500

    Added user path input for music

--- CrimssoundPlayer/Program.cs
+++ CrimssoundPlayer/Program.cs
@@ -3,13 +3,17 @@ using NAudio.Wave;      // Imports NAudio.Wave, to access audio-related classes
 class Program{
     static void Main()  // The entry point of the application, alongside Class Program
     {
-        var testFile = new AudioFileReader("D:\\Music\\Blue Blur- Sonic Generations Original Soundtrack\\Disc 2\\08 Radical Highway- Act 1 'Vengeance Is Mine - Cash Cash RMX'.mp3"); // Creates an audio reader for the file
+        Console.WriteLine("Enter song path: ");
+        string songPath = Console.ReadLine();
+        //var testFile = new AudioFileReader("D:\\Music\\Blue Blur- Sonic Generations Original Soundtrack\\Disc 2\\08 Radical Highway- Act 1 'Vengeance Is Mine - Cash Cash RMX'.mp3"); // Creates an audio reader for the file
+        var testFile = new AudioFileReader(songPath);
         var outputDevice = new WaveOutEvent();  // Creates a playback device that sends sound to my default audio output
         outputDevice.Init(testFile);            // Links testFile with the outputDevice
         outputDevice.Play();                    // Starts playing audio
         Console.WriteLine("Music is playing. Press P to pause, S to stop, and any other key to exit."); // Displays message active while music is playing
         bool isPaused = false;
         while(true){
+            Console.WriteLine("");
             var userInput = Console.ReadKey(true).Key;
             if(userInput == ConsoleKey.P){
                 if(isPaused && testFile.Position == 0){
@@ -37,3 +41,5 @@ class Program{
         outputDevice.Stop(); // Stops music
     }
 }
+// D:\Music\Blue Blur- Sonic Generations Original Soundtrack\Disc 2\08 Radical Highway- Act 1 'Vengeance Is Mine - Cash Cash RMX'.mp3
+// D:\Music\Blue Blur- Sonic Generations Original Soundtrack\Disc 2\10 Water Palace- Act 1 'Back 2 Back - Digital Remakin' Track'.mp3
